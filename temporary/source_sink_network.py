97d18cd80bdd2eb8df548cf799b8640edbfe16da
wangweiran0129 <wangwr0129@gmail.com>
2021-12-11 00:33:38 +0100

    Fixed the bugs of incorrect src and dest for flow information

--- temporary/source_sink_network.py
+++ temporary/source_sink_network.py
@@ -4,7 +4,7 @@
 import csv
 import networkx as nx
 
-def source_sink_server(servers):
+def source_sink_construction(servers):
     
     '''
     Construct a source-sink network servers
@@ -18,6 +18,7 @@ def source_sink_server(servers):
     topology_id = str(servers)
     server_rate = 10
     server_latency = 0.001
+    servers_size = servers + 1
 
     # creaet a csv file for server
     path_server = '/Users/wangweiran/Desktop/SemesterProject/EPFL_Network_Calculus_Semester_Project/server_info/'
@@ -30,39 +31,43 @@ def source_sink_server(servers):
 
         # write the columns names
         server_csv.writerow(['topology_id', 'server_id', 'server_rate', 'server_latency'])
-        for i in range(servers):
-            server_csv.writerow([topology_id, i+1, server_rate, server_latency])
+        for i in range(servers_size):
+            server_csv.writerow([topology_id, i, server_rate, server_latency])
 
-
-def source_sink_flow(servers):
     '''
     flows bursts, b = 1kb
     flow rates depneds on the load r = (R*0.5)/servers
     flow of interest (foi) crosses all servers
     '''
-    topology_id = str(servers)
-    flows_number = 2 * servers - 1
+    #topology_id = str(servers)
+    flows_number = 2 * servers_size - 1
     network_id = []
     flows_bursts = 10
-    flows_rates = 5 / servers
+    flows_rates = 5 / servers_size
     flows_src = []
     flows_dest = []
     foi = []
 
     # define the src & dest of a flow
-    for src in range(1, servers+1):
-        for dest in range(src, servers+1):
-            flows_src.append(src)
-            flows_dest.append(dest)
+    # 1st category : source is the first server and the destination changes
+    for dest in range(servers_size):
+        flows_src.append(0)
+        flows_dest.append(dest)
+    
+    # 2nd category : destination is the last server and sources change
+    # exclude src = 0, dest = servers_size-1 which has been calculated before in the first category
+    for src in range(1,servers_size):
+        flows_src.append(src)
+        flows_dest.append(servers_size-1)
 
     # set -1 for foi in the base graph
     for i in range(flows_number):
         foi.append(-1)
 
     # define the flow of interest
-    for networkID in range(1, flows_number+1):
+    for networkID in range(flows_number):
         network_id.append(networkID)
-        for flow in range(1, flows_number+1):
+        for flow in range(flows_number):
             if flow == networkID:
                 foi.append(1)
             else:
@@ -83,14 +88,14 @@ def source_sink_flow(servers):
         for i in range(flows_number+1):
             # loop for flows
             for j in range(flows_number):
-                flow_csv.writerow([topology_id, i, j+1, flows_rates, flows_bursts, flows_src[j], flows_dest[j], foi[j+flows_number*i]])
- 
+                flow_csv.writerow([topology_id, i, j, flows_rates, flows_bursts, flows_src[j], flows_dest[j], foi[j+flows_number*i]])
+
+
 
 def main():
     server_num = 25
     for server in range(server_num):
-        source_sink_server(server + 1)
-        source_sink_flow(server + 1)
+        source_sink_construction(server)
 
 
 if __name__ == "__main__":
