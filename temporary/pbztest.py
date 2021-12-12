c4b95b584abcddaa1cda60f27fd5772082f3f3f9
wangweiran0129 <wangwr0129@gmail.com>
2021-12-12 12:50:44 +0100

    Successfully had a test on source sink network with two servers

--- temporary/pbztest.py
+++ temporary/pbztest.py
@@ -2,7 +2,7 @@ from pbzlib import write_pbz, open_pbz
 import sys
 sys.path.append(r'/Users/wangweiran/Desktop/SemesterProject/EPFL_Network_Calculus_Semester_Project/DeepFP_gnn-main/')
 from src.extraction.tests.source_sink_pb2 import Network, Server
-from tests import source_sink_pb2, messages_pb2
+from tests import source_sink_pb2
 
 def main():
     objs = [source_sink_pb2.Network(id=0)]
@@ -14,12 +14,28 @@ def main():
     server.id = 0
     server.rate = 10
     server.latency = 0.001
+    server = objs[0].server.add()
+    server.id = 1
+    server.rate = 10
+    server.latency = 0.001
 
     flow = objs[0].flow.add()
     flow.id = 0
     flow.rate = 5
     flow.burst = 10
     flow.path.append(0)
+    flow = objs[0].flow.add()
+    flow.id = 1
+    flow.rate = 5
+    flow.burst = 10
+    flow.path.append(0)
+    flow.path.append(1)
+    flow = objs[0].flow.add()
+    flow.id = 2
+    flow.rate = 5
+    flow.burst = 10
+    flow.path.append(1)
+    
     
     with write_pbz("output.pbz", "tests/source_sink.descr") as w:
         for obj in objs:
